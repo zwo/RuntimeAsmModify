bffd9c279bc3870cb077c43667964f31e04c2500
zhouweiou <zhouweiou@rfchina.com>
2018-12-12 11:25:04 +0800

    Initial Commit

---
+++ RuntimeAsmModify/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RuntimeAsmModify
+//
+//  Created by 周维鸥 on 2018/12/12.
+//  Copyright © 2018年 周维鸥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
